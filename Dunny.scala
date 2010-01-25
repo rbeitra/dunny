f28e1b3593a9c2184a0a29b0e65aab00c8327dde
jpike <jpike@afterclap.com>
2010-01-25 11:33:28 +0000

    add some operator overloading stuff

--- Dunny.scala
+++ Dunny.scala
@@ -1,70 +1,71 @@
+// vim:smarttab:ts=4 sw=4
 package org.chilon.dunny
 
 import java.io.DataOutputStream
 
-class Source{
-	def step(time: Double): Double = {
-		return 0
-	}
+class Source {
+    def step(time: Double): Double = 0
+
+    def +(s: Source) = new Add(this, s)
+    def +(s: Double) = new Add(this, new Constant(s))
+
+    def *(s: Source) = new Mul(this, new Constant(s))
+    def *(s: Double) = new Mul(this, new Constant(s))
 }
 class Constant(v: Double) extends Source{
-	var value = v
-	override def step(time: Double): Double = {
-		return value
-	}
+    var value = v
+    override def step(time: Double) = value
 }
 class Phasor(f: Source) extends Source{
-	var frequency = f
-	var phase: Double = 0
-	override def step(time: Double): Double = {
-		phase = (phase + frequency.step(time)*time)
-		return phase
-	}
+    var frequency = f
+    var phase: Double = 0
+    override def step(time: Double): Double = {
+        phase = (phase + frequency.step(time)*time)
+        return phase
+    }
 }
 class Saw(p: Source) extends Source{
-	var phase = p
-	override def step(time: Double): Double = {
-		return (phase.step(time)%1)*2-1
-	}
+    var phase = p
+    override def step(time: Double): Double = (phase.step(time)%1)*2-1
 }
 class Sin(p: Source) extends Source{
-	var phase = p
-	override def step(time: Double): Double = {
-		return Math.sin(phase.step(time)*2*Math.Pi)
-	}
+    var phase = p
+    override def step(time: Double): Double = {
+        return Math.sin(phase.step(time)*2*Math.Pi)
+    }
 }
 class Sqr(p: Source) extends Source{
-	var phase = p
-	override def step(time: Double): Double = {
-		return if(phase.step(time)%1<0.5) -1 else 1
-	}
+    var phase = p
+    override def step(time: Double): Double = {
+        return if(phase.step(time)%1<0.5) -1 else 1
+    }
 }
 class Add(a:Source, b:Source) extends Source{
-	var sourcea = a
-	var sourceb = b
-	override def step(time: Double): Double = {
-		return sourcea.step(time) + sourceb.step(time)
-	}
+    var sourcea = a
+    var sourceb = b
+    override def step(time: Double): Double = {
+        return sourcea.step(time) + sourceb.step(time)
+    }
 }
 class Mul(a:Source, b:Source) extends Source{
-	var sourcea = a
-	var sourceb = b
-	override def step(time: Double): Double = {
-		return sourcea.step(time) * sourceb.step(time)
-	}
+    var sourcea = a
+    var sourceb = b
+    override def step(time: Double): Double = {
+        return sourcea.step(time) * sourceb.step(time)
+    }
 }
 class Chromatic(s:Source) extends Source{
-	var source = s
-	override def step(time: Double): Double = {
-		return Math.pow(2, (source.step(time)/12)+7)
-	}
+    var source = s
+    override def step(time: Double): Double = {
+        return Math.pow(2, (source.step(time)/12)+7)
+    }
 }
 class Sequencer(p: Source, s: Sequence) extends Source{
-	var phase = p
-	var sequence = s
-	override def step(time: Double): Double = {
-		return sequence.discrete(phase.step(time))
-	}
+    var phase = p
+    var sequence = s
+    override def step(time: Double): Double = {
+        return sequence.discrete(phase.step(time))
+    }
 }
 
 class Sample (b: Double){
@@ -103,28 +104,23 @@ object Dunny {
         var key = new Sequence(Array(0, 3, -2, 1), 0.25)
         var prev: Double = 0
         
-		var output =
-			add(
-				sqrwave(
-					chromatic(
-						add(
-							seq(key),
-							seq(notes)
-						)
-					)
-				),
-				sawwave(
-					mul(
-						chromatic(
-							add(
-								seq(key),
-								seq(notes2)
-							)
-						),
-						const(2)
-					)
-				)
-			)
+        var output =
+            sqrwave(
+                chromatic(
+                    add(
+                        seq(key),
+                        seq(notes)
+                    )
+                )
+            ) +
+            sawwave(
+                chromatic(
+                    add(
+                        seq(key),
+                        seq(notes2)
+                    )
+                ) * 2
+            )
 
         while (s.increment < LENGTH) {
             var mix: Double = 0
@@ -134,15 +130,13 @@ object Dunny {
             prev = s.time
         }
     }
-    
        
-	def const(v: Double): Source = return new Constant(v);
+    def const(v: Double): Source = return new Constant(v);
     def sawwave(freq: Source): Source = return new Saw(new Phasor(freq))
     def sqrwave(freq: Source): Source = return new Sqr(new Phasor(freq))
     def sinwave(freq: Source): Source = return new Sin(new Phasor(freq))
     def add(a: Source, b: Source): Source = return new Add(a, b);
-    def mul(a: Source, b: Source): Source = return new Mul(a, b);
     def chromatic(pitch: Source): Source = return new Chromatic(pitch);
     def noteseq(notes: Sequence): Source = return chromatic(new Sequencer(new Phasor(new Constant(1)), notes))
-	def seq(notes: Sequence): Source = return new Sequencer(new Phasor(new Constant(1)), notes)
+    def seq(notes: Sequence): Source = return new Sequencer(new Phasor(new Constant(1)), notes)
 }
