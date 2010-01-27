6eb4b7d135366b056ff73f5d844233508964bab4
rbeitra <robin@beitra.net>
2010-01-27 14:55:18 +0000

    added addressable stuff

--- Sequence.scala
+++ Sequence.scala
@@ -1,15 +1,35 @@
 package org.chilon.dunny
 
-class Sequence(val sequence: Array[Float], val rate: Float) {
-    def discrete(time: Float) = sequence((time*rate).toInt%sequence.length)
+class Addressable() {
+    def discrete(time: Float): Float = 0;
+    def linear(time: Float): Float = 0;
+}
 
-    def linear(time: Float): Float = {
+class Sequence(val sequence: Array[Float], val rate: Float) extends Addressable {
+    override def discrete(time: Float): Float = sequence((time*rate).toInt%sequence.length)
+
+    override def linear(time: Float): Float = {
         var position = sequence((time*rate).toInt % sequence.length)
         var next = sequence((time*rate + 1).toInt % sequence.length)
         var ratio = (time * rate) % 1
 
         (position*(1-ratio) + next*ratio)
     }
+}
+
+class Slice(val source: Addressable, val start: Float, val end: Float) extends Addressable {
+    override def discrete(time: Float): Float = if(time >= start && time < end) source.discrete(time + start) else 0;
+    override def linear(time: Float): Float = if(time >= start && time < end) source.linear(time + start) else 0;
+}
+
+class Loop(val source: Addressable, val start: Float, val end: Float) extends Addressable {
+    val length = end - start;
+    override def discrete(time: Float): Float = source.discrete(start + (time - start)%length)
+    override def linear(time: Float): Float = source.linear(start + (time - start)%length)
+}
+class Appended(val sourcea: Addressable, val sourceb: Addressable, val switchtime: Float) extends Addressable {
+    override def discrete(time: Float): Float = if(time < switchtime) sourcea.discrete(time) else sourceb.discrete(time - switchtime)
+    override def linear(time: Float): Float = if(time < switchtime) sourcea.linear(time) else sourceb.linear(time - switchtime)
 }
 
 object Sequence {
